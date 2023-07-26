9dfbf4670b393d65a2eb79def88fdc4afdf1a1ee
Machadoggg <131879710+Machadoggg@users.noreply.github.com>
2023-07-26 01:53:34 -0500

    Add DataContext and first migration

---
+++ APIRest_WEBBlazor/APIRest_WEBBlazor.API/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using APIRest_WEBBlazor.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace APIRest_WEBBlazor.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //Creacion de index
+            modelBuilder.Entity<Customer>().HasIndex(x => x.Name).IsUnique();
+        }
+    }
+}
---
+++ APIRest_WEBBlazor/APIRest_WEBBlazor.API/Migrations/20230726064918_InitialDB.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APIRest_WEBBlazor.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Customers_Name",
+                table: "Customers",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
